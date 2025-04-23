5b38663507dfb1ffdd92bf6e235a6d8f2059cb19
Ericson Castasus <castasuse@gmail.com>
2025-04-24 00:22:14 +0800

    fix responsiveness auth screen

--- app/(auth-pages)/layout.tsx
+++ app/(auth-pages)/layout.tsx
@@ -10,17 +10,17 @@ export default async function Layout({
   return (
     <div className="max-w-7xl min-h-screen grid content-center mx-auto">
       <div>
-        <div className="grid grid-cols-6">
-          <div className="col-span-2">
+        <div className="md:grid md:grid-cols-6 flex flex-col items-center">
+          <div className="md:col-span-3 mb-2 md:flex md:justify-center">
             <Image
               src={appName}
               alt="App Name"
               width={720}
               height={720}
-              className="w-[1000px]"
+              className="w-[170px] md:w-[400px]"
             />
           </div>
-          <div className="col-span-4 mx-auto my-auto flex flex-col items-start">
+          <div className="md:col-span-3 mx-auto my-auto flex flex-col items-start">
             {children}
             <div className="flex items-center gap-3">
               <p className="text-sm">Change theme</p>
